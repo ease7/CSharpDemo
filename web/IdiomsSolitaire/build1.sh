e115e5229a8da4fa3a85aeba0f10b413bb8db5b0
luquan <easeven@126.com>
2019-12-19 20:55:37 +0800

    udpate

--- web/IdiomsSolitaire/build1.sh
+++ web/IdiomsSolitaire/build1.sh
@@ -2,6 +2,8 @@ dotnet restore IdiomsSolitaire.Blazor/IdiomsSolitaire.Blazor.csproj
 
 dotnet publish IdiomsSolitaire.Blazor/IdiomsSolitaire.Blazor.csproj -c Release -o /usr/app
 
+
 export db='server=localhost;port=3306;user=root;password=Password01!;database=test;'
 
-# nohup dotnet IdiomsSolitaire.Blazor.dll &
+
+## nohup dotnet IdiomsSolitaire.Blazor.dll &
