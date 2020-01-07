a2744e149da7e95e7ea04d8829cb0497923378bb
easeven <easeven@126.com>
2020-01-07 11:23:58 +0800

    baidu.ai

---
+++ src/Baidu.AIDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace Baidu.AIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // 设置APPID/AK/SK
+            var APP_ID = "18191384";
+            var API_KEY = "--";
+            var SECRET_KEY = "--";
+
+            var client = new Baidu.Aip.Nlp.Nlp(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+            
+
+            var text = "我们知道深度学习中对图像处理应用最好的模型是CNN";
+
+            // 调用词法分析，可能会抛出网络等异常，请使用try/catch捕获
+            var result = client.LexerCustom(text);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
